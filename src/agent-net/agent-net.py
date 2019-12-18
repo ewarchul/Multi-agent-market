ed5b79416f52fa464e929ae8fa3cd93d77450eb5
warbarbye <ewarchul@gmail.com>
2019-12-18 11:30:19 +0100

    Add class struct of agent network

---
+++ src/agent-net/agent-net.py
@@ -0,0 +1,29 @@
+class AgentNet(object):
+    """
+    Class with agent network graph.
+    """
+    def __init__(self):
+        self.agent_num =  0
+    def create_network(self):
+        pass
+    def add_agent(self):
+        pass
+    def remove_agent(self):
+        pass
+    def rearange(self):
+        pass
+    def show_network(self):
+        pass
+    def save_network(self):
+        pass
+    def load_network(self):
+        pass
+
+class RandomAgentNet(AgentNet):
+    def __init__(self):
+        self.initial_connected = 0
+        self.prob_func = 0
+
+class FullyAgentNet(AgentNet):
+    pass
+
